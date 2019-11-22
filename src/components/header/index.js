baf93c62e8303ce23d4e1d63f68e6661dddfea2c
Luiza <luizapereiratvd@gmail.com>
2019-11-22 17:06:40 -0300

    Adding Header component

---
+++ src/components/header/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+export default class Header extends Component {
+  state = {
+    routes: [
+      { path: '/', title: 'Home' },
+      { path: '/characters', title: 'Characters' },
+      { path: '/planets', title: 'Planets' },
+      { path: '/species', title: 'Species' },
+      { path: '/spaceships', title: 'Spaceships' },
+      { path: '/vehicles', title: 'Vehicles' },
+      { path: '/about', title: 'About' },
+    ],
+  };
+
+  render() {
+    const { routes } = this.state;
+    return (
+      <div className="title-bar">
+        <h1>STAR WARS</h1>
+        <nav>
+          <ul>
+            {routes.map(route => (
+              <li>
+                <a href={route.path}>{route.title}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
