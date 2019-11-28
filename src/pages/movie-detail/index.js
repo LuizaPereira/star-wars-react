02fbb4d59165010998f72891a91042a2ab3fa1e9
Luiza <luizapereiratvd@gmail.com>
2019-11-28 16:21:59 -0300

    Adding Style to movie-detail

--- src/pages/movie-detail/index.js
+++ src/pages/movie-detail/index.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function Movie() {
+import api from './../../services/api';
+import './styles.scss';
+
+export default function Movie(props) {
+  const [movie, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await api.get(`/films`);
+      const { results } = response.data;
+      const m = results.find(movie => {
+        return `${movie.episode_id}` === `${props.match.params.id}`;
+      });
+      setMovies(m);
+    }
+    fetchData();
+  }, [props.match.params.id]);
+
+  const fetchDetails = details => {
+    details && details.map(detail => {});
+
+    // console.log(details);
+    return (
+      <li>
+        <p>batata</p>
+      </li>
+    );
+  };
+
   return (
     <div className="container">
-      <h1> BATATA </h1>
+      <h1>
+        Episode {movie.episode_id} : {movie.title}
+      </h1>
+      <div className="sub-title">
+        <p>Release Date: {movie.release_date}</p>
+        <p>Director : {movie.director}</p>
+        <p>Producer : {movie.producer}</p>
+      </div>
+      <div className="movie-crawl">
+        <span>{movie.opening_crawl}</span>
+      </div>
+
+      <div className="characters-block">
+        <h1>Characters</h1>
+        <ul className="characters-list">{fetchDetails(movie.characters)}</ul>
+      </div>
     </div>
   );
 }
